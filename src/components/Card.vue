849d1a9db868b1c4504e0488b7f38563fab69bce
ChenChingHsin <chinghsinforwork@gmail.com>
2022-04-06 23:14:55 +0800

    card design

---
+++ src/components/Card.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card-item col-lg-3 col-md-4 col-6">
+    <div class="card-upper">
+      <img
+        calss="img-fluid"
+        :src="product.imageUrl"
+        alt=""
+      />
+      <p class="card-tag" v-if="product.price !== product.origin_price">
+        ON SALE
+      </p>
+      <a class="card-favicon" href="">
+        <i class="bi bi-heart-fill" v-if="myFavorite.includes(product.id)"></i>
+        <i class="bi bi-heart" v-else></i>
+      </a>
+      <div class="card-detail pt-4">
+        <router-link :to="`/product/${product.id}`" class="card-linkstyle">
+          詳細商品資訊
+          <i class="bi bi-arrow-right"></i>
+        </router-link>
+      </div>
+      <div class="card-soldout" v-if="is_enabled == true">
+        <h5 class="card-soldouttag">SOLD OUT</h5>
+      </div>
+    </div>
+    <div class="card-info">
+      <h6 class="card-title"> {{ product.title}} </h6>
+      <h7 class="card-pricetag">NT$ {{ product.price }}
+        <span class="card-orgprice ml-2"
+          v-if="product.price !== product.origin_price">$ {{ product.origin_price}}
+        </span>
+      </h7>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: Object,
+    myFavorite: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
--- src/views/frontend/ProductsView.vue
+++ src/views/frontend/ProductsView.vue
@@ -1,68 +1,28 @@
 <template>
   <h2>產品列表</h2>
   <div class="container">
-    <div class="row row-cols-1 row-cols-lg-4 g-3">
-      <!-- row決定內層的數量-->
-      <div class="col" v-for="product in products" :key="product.id">
-        <div class="card h-100">
-          <img :src="product.imageUrl" class="card-img-top" alt="" />
-          <div class="card-body">
-            <h5 class="card-title">{{ product.title }}</h5>
-            <p class="card-text">{{ product.discription }}</p>
-            <router-link :to="`/product/${product.id}`" class="btn btn-primary"
-              >詳細內容</router-link
-            >
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="container">
     <div class="row">
-      <div class="col-md-3">1233211234567</div>
+      <div class="col-md-3">
+        <ul class="d-flex flex-wrap flex-md-nowrap d-xl-block justify-content-md-center">
+          <li class="category-link mb-xl-4">全部商品</li>
+          <li>我的收藏</li>
+          <hr>
+          <li>乳酪蛋糕</li>
+          <li>鮮奶油蛋糕</li>
+          <li>磅蛋糕</li>
+          <hr>
+          <li>手作餅乾</li>
+          <li>司康</li>
+          <li>其他產品</li>
+          <hr>
+          <li>優惠組合</li>
+        </ul>
+      </div>
       <div class="col-md-9">
-        <div class="row">
-          <div
-            class="col-lg-3 col-md-4 col-6"
-            v-for="product in products"
-            :key="product.id + 'product'"
-          >
-            <div class="card border-0 mb-4 position-relative position-relative">
-              <a
-                href="#"
-                class="btn btn-sm btn-secondary text-white secondary-hover me-2"
-              >
-                <i class="bi bi-heart"></i>
-              </a>
-              <a
-                href="#"
-                class="
-                  btn btn-sm btn-outline-secondary
-                  white-hover
-                  cartCard-btn
-                  me-2
-                "
-              >
-                <i class="bi bi-heart-fill"></i>
-              </a>
-              <img :src="product.imageUrl" class="card-img-top" alt="..." />
-              <a href="#" class="text-dark">
-                <i
-                  class="far fa-heart position-absolute"
-                  style="right: 16px; top: 16px"
-                ></i>
-              </a>
-              <div class="card-body p-0 text-center">
-                <h5 class="mb-0 mt-3">
-                  <a href="#">{{ product.title }}</a>
-                </h5>
-                <p class="card-text text-muted mb-0">
-                  Lorem ipsum dolor sit amet
-                </p>
-                <p class="text-muted mt-3">NT${{ product.price }}</p>
-              </div>
-            </div>
-          </div>
+        <div class="row gx-5 gy-3">
+          <template v-for="item in products" :key="item.id + 'card'">
+            <Card :product="item"></Card>
+          </template>
         </div>
       </div>
     </div>
@@ -70,7 +30,12 @@
 </template>
 
 <script>
+import Card from '@/components/Card.vue';
+
 export default {
+  components: {
+    Card,
+  },
   data() {
     return {
       products: [],
